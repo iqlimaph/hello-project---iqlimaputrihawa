c419286153488991f9e455dce3c53da026d7af8e
iqlima <you@example.com>
2022-03-17 20:46:23 +0700

    luas

---
+++ luas.py
@@ -0,0 +1,10 @@
+def hitung_luas_lingkaran(r):
+    luas = 3.14 * r**2
+    return luas
+
+def hitung_luas_persegi(s):
+    luas = s*s
+    return luas
+
+print('luas lingkaran dengan diameter 7 =', hitung_luas_lingkaran(7))
+print('luas persegi dengan panjang sisi 4 =', hitung_luas_persegi(4))
